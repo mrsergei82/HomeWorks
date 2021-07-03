cbea034d563b7c372779a238875e85bd3a1e40ea
Sergei <mr.sergei82@mail.ru>
2021-07-03 08:12:26 +0300

    первое задание

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -1,3 +1,14 @@
 public class Lesson3 {
+    public static void main(String[] args) {
+        int[] ints = {1,1,0,0,1,0,1,1,0,1};
+        int[] ints1 = new int[10];
+        for (int i = 0; i<ints.length; i++){
+            if (ints[i] == 0){
+                ints1[i] = 1;
+            }
+            else ints1[i] = 0;
+            System.out.println(ints1[i]);
+        }
+    }
 
 }
