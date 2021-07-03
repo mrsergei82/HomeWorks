b9ca3b30099d63968ec8041f85eec4938b68deac
Sergei <mr.sergei82@mail.ru>
2021-07-03 10:00:11 +0300

    шестое задание

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -9,11 +9,13 @@ public class Lesson3 {
             else ints1[i] = 0;
             System.out.println(ints1[i]);
         }
+        // второе
         int[] ints2 = new int[100];
         for (int i = 0; i < ints2.length; i++){
             ints2[i] = i+1;
             System.out.println(ints2[i]);
         }
+        // третье
         int[] ints3 = {1,5,3,2,11,4,5,2,4,8,9,1};
         int[] ints31 = new int[12];
         for (int i = 0; i < ints3.length; i++){
@@ -23,6 +25,7 @@ public class Lesson3 {
             else ints31[i] = ints3[i];
             System.out.println(ints31[i]);
         }
+        // четвёртое
         int[][] ints4 = new int[5][5];
         for (int i = 0; i < 5; i++){
             for (int j = 0; j < 5; j++){
@@ -41,8 +44,24 @@ public class Lesson3 {
             }
         }
         massiv(5, 3);
+        //шестое
+        int[] compare = {3,4,-8,2,-1};
+        int min = 9999;
+        int max = 0;
+        for (int i = 0; i<compare.length; i++){
+            if (compare[i]>max){
+                max=compare[i];
+            }else max=max;
+        }
+        for (int i = 0; i<compare.length; i++){
+            if (compare[i]<min){
+                min = compare[i];
+            }else min = min;
+        }
+        System.out.println(min + ", "+max );
 
     }
+    // пятое задание
     public static int[] massiv(int a , int b){
         int[] ints = new int[a];
         for (int i = 0; i<a; i++){
@@ -51,5 +70,6 @@ public class Lesson3 {
 
         return ints;
     }
+
 
 }
