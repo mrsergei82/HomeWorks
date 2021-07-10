eee21a1e0041d12a46e0c0f44e072fd10e9c8eb7
Sergei <mr.sergei82@mail.ru>
2021-07-10 10:28:04 +0300

    Переделал класс Работник

---
+++ src/Employee.java
@@ -0,0 +1,19 @@
+public class Employee {
+     String name;
+     String position;
+     String email;
+     String phoneNumber;
+     int salary;
+     int age;
+
+    public Employee(String name,String position,String email,String phoneNumber,int salary,int age){
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+    public void print(){
+        System.out.println(name+" "+position+" "+email+" "+phoneNumber+" "+salary+age);}
+}
--- src/Lesson5.java
+++ src/Lesson5.java
@@ -1,40 +1,22 @@
 public class Lesson5 {
     public static void main(String[] args){
-        Person[] persons = new Person[5];
-        persons[0]= new Person("Ivan","povar","i@van","003",30000,30);
-        persons[1]= new Person("Vova","slesar","v@ova","004",40000,45);
-        persons[2]= new Person("Katia","sekretar","k@tia","005",25000,20);
-        persons[3]= new Person("Dima","security","d@ima","007",50000,38);
-        persons[4]= new Person("Nikolai","direktor","ni@ko","008",1000,50);
+        Employee[] employees = new Employee[5];
+        employees[0]= new Employee("Ivan","povar","i@van","003",30000,30);
+        employees[1]= new Employee("Vova","slesar","v@ova","004",40000,45);
+        employees[2]= new Employee("Katia","sekretar","k@tia","005",25000,20);
+        employees[3]= new Employee("Dima","security","d@ima","007",50000,38);
+        employees[4]= new Employee("Nikolai","direktor","ni@ko","008",1000,50);
 
-        for (int i=0;i< persons.length;i++){
-            if (persons[i].age>=40){
-                persons[i].print();
+        for (int i=0;i< employees.length;i++){
+            if (employees[i].age>=40){
+                employees[i].print();
             }
 
         }
     }
 
-    public static class Person {
-        private String name;
-        private String position;
-        private String email;
-        private String phoneNumber;
-        private int salary;
-        private int age;
 
-        public Person(String name,String position,String email,String phoneNumber,int salary,int age){
-            this.name = name;
-            this.position = position;
-            this.email = email;
-            this.phoneNumber = phoneNumber;
-            this.salary = salary;
-            this.age = age;
-        }
-        public void print(){
-            System.out.println(name+" "+position+" "+email+" "+phoneNumber+" "+salary+age);
-        }
 
-    }
+
 }
 
